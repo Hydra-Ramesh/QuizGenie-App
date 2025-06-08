926f42a48d34dfa2e9b8bb325740c63833e0169c
Hydra-Ramesh <dasramesh8343003905@gmail.com>
2025-06-08 23:50:01 +0530

    Backend is Ready for Testing

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import quizRoute from "./routes/quizRoute.js";
+import evaluateRoute from "./routes/evaluationRoute.js";
 dotenv.config();
 
 const app = express();
@@ -8,6 +10,9 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/v1", quizRoute);
+app.use("/api/v1", evaluateRoute);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
